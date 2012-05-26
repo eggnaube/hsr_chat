9e498d4c0921ec9e136654c42347e1f6f0db3fc9
Benno Eggnauer <b.eggnauer@swissonline.ch>
2012-05-27 00:28:27 +0200

    Auto scroll

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -2,3 +2,7 @@ $('#hsr_chat_tab a').click(function(e) {
 	e.preventDefault();
 	$(this).tab('show');
 });
+
+$(document).ready(function() {
+	$("#HC_chat_messages").prop({ scrollTop: $("#HC_chat_messages").prop('scrollHeight') });  
+});
